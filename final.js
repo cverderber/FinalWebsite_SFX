93f828d2e1de93c9d481fb068248d847956d983f
Cole Verderber <cverderber@me.com>
2015-12-09 11:36:26 -0500

    All Website files
    All website files..

---
+++ final.js
@@ -0,0 +1,101 @@
+  function play(){
+       var audio = document.getElementById("note");
+       audio.play();     
+                 }			 
+ 
+  function play2(){
+	   var audio = document.getElementById("note2");
+       audio.play();     
+                 }		
+				 	 
+  function play3(){
+	   var audio = document.getElementById("note3");
+       audio.play();     
+                 }	
+				 			 	 
+  function play4(){
+	   var audio = document.getElementById("note4");
+       audio.play();     
+                 }	
+				 
+   function play5(){
+	   var audio = document.getElementById("note5");
+       audio.play();     
+                 }	
+				 			 	 
+  function play6(){
+	   var audio = document.getElementById("note6");
+       audio.play();     
+                 }	
+				 
+  function play7(){
+       var audio = document.getElementById("note7");
+       audio.play();     
+                 }			 
+ 
+  function play8(){
+	   var audio = document.getElementById("note8");
+       audio.play();     
+                 }		
+				 	 
+  function play9(){
+	   var audio = document.getElementById("note9");
+       audio.play();     
+                 }	
+				 			 	 
+  function play17(){
+	   var audio = document.getElementById("note17");
+       audio.play();     
+                 }	
+				 			 	 
+  function play11(){
+	   var audio = document.getElementById("note11");
+       audio.play();     
+                 }	
+				 
+  function play12(){
+       var audio = document.getElementById("note12");
+       audio.play();     
+                 }			 
+ 
+  function play13(){
+	   var audio = document.getElementById("note13");
+       audio.play();     
+                 }		
+				 	 
+  function play14(){
+	   var audio = document.getElementById("note14");
+       audio.play();     
+                 }	
+				 			 	 
+  function play15(){
+	   var audio = document.getElementById("note15");
+       audio.play();     
+                 }	
+				 			 	 
+  function play16(){
+	   var audio = document.getElementById("note16");
+       audio.play();     
+                 }	
+				 
+				 
+				 			 
+  function coin1(){
+	  var audio = document.getElementById("coin1");
+	  audio.play();
+  }
+  
+	  function coined(){
+	  var audio = document.getElementById("coined");
+	  audio.play();
+  }		
+  
+    function mario(){
+	  var audio = document.getElementById("mario");
+	  audio.play();
+  }
+  
+  
+  
+  
+	 
